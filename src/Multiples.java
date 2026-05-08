b01385bf4fa571b54111fc5e114679496fdfb1ae
selinage <selina.ge@mail.utoronto.ca>
2026-05-07 20:46:09 -0400

    completed lab tasks

---
+++ src/Multiples.java
@@ -0,0 +1,17 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 0; i < 1000; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+
+    }
+}
